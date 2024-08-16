310105b9f2304f62f906c70cc95b7d90bf4a6806
touhou09 <touhou09>
2024-08-16 15:07:05 +0000

    s3 settings

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+from dotenv import load_dotenv
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, split, col
+from pyspark.sql.functions import col
 import PyPDF2
+
+load_dotenv()
 
 # Spark 세션 생성
 spark = SparkSession.builder \
     .appName("PDF Word Count") \
+    .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID")) \
+    .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY")) \
+    .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
     .getOrCreate()
 
 # 현재 파일의 경로를 기반으로 PDF 파일 경로 설정
@@ -33,11 +39,14 @@ words = rdd.flatMap(lambda line: line.split())
 # 단어를 DataFrame으로 변환
 df = words.map(lambda word: (word, )).toDF(["word"])
 
-# 단어 갯수 세기 및 단어 갯수 순으로 정렬
-word_counts = df.groupBy("word").count().orderBy(col("count").desc(), col("word"))
+# 단어 갯수 세기 및 단어 갯수 순으로 정렬, 단어 갯수가 같을 경우 단어 알파벳 순으로 정렬
+word_counts = df.groupBy("word").count().orderBy(col("count").desc(), col("word").asc())
 
 # 모든 결과값 출력
 word_counts.show(truncate=False, n=word_counts.count())
+
+output_path = "s3a://word-counter/word_count_output/"
+word_counts.write.csv(output_path, header=True)
 
 # 세션 종료
 spark.stop()
